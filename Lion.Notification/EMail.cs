36e594ccc612d49e87c27184cb6f2f16b6c4cc0e
giangkool <giangkoolpark@gmail.com>
2017-01-05 20:37:09 +0700

    update notification & sercurity check ip server

---
+++ Lion.Notification/EMail.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lion.Notification
+{
+    public class EMail
+    {
+        /// <summary>
+        /// Send email notification
+        /// </summary>
+        /// <param name="sub"></param>
+        /// <param name="content"></param>
+        /// <param name="mailto"></param>
+        /// <param name="mailfrom"></param>
+        /// <param name="mailfrompassword"></param>
+        public static void SendMail(string sub, string content, string mailto, string mailfrom, string mailfrompassword)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                client.Port = 587;
+                client.Host = "smtp.gmail.com";
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential(mailfrom, mailfrompassword);
+
+                MailMessage mail = new MailMessage(mailfrom, mailto, sub, content);
+                mail.BodyEncoding = UTF8Encoding.UTF8;
+                mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+                client.Send(mail);
+            }
+            catch (Exception ex) { ex.ToString(); }
+        }
+    }
+}
---
+++ Lion.Sercurity/CheckServerIp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lion.Sercurity
+{
+    public class CheckServerIp
+    {
+        // check your ip
+        public static string Checkip(string server_ip, string your_ip)
+        {
+            if (server_ip == your_ip)
+            {
+                return "OK";
+            }
+            return "FAlSE";
+        }
+    }
+}
